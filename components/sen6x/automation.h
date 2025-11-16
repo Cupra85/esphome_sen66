cf3b4f3f1a5ee77f74d4ee89b1d40d4d4fd0002a
Cupra85 <daniel.curten@gmail.com>
2025-11-16 15:58:09 +0100

    Update automation.h

--- components/sen6x/automation.h
+++ components/sen6x/automation.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
 #include "sen6x.h"
@@ -10,9 +9,23 @@ namespace sen6x {
 template<typename... Ts> class StartFanAction : public Action<Ts...> {
  public:
   explicit StartFanAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
-
   void play(Ts... x) override { this->sen6x_->start_fan_cleaning(); }
+ protected:
+  SEN5XComponent *sen6x_;
+};
 
+template<typename... Ts> class StopMeasurementAction : public Action<Ts...> {
+ public:
+  explicit StopMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
+  void play(Ts... x) override { this->sen6x_->stop_measurement(); }
+ protected:
+  SEN5XComponent *sen6x_;
+};
+
+template<typename... Ts> class StartMeasurementAction : public Action<Ts...> {
+ public:
+  explicit StartMeasurementAction(SEN5XComponent *sen6x) : sen6x_(sen6x) {}
+  void play(Ts... x) override { this->sen6x_->start_measurement(); }
  protected:
   SEN5XComponent *sen6x_;
 };
